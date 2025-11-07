e32db5d2242f8b426961c45ae3a3b5c10b850482
Sam Ford <1584702+samford@users.noreply.github.com>
2025-11-07 14:32:30 -0500

    rdkit 2025.09.2
    `rdkit` is autobumped but the workflow failed to update to version
    2025.09.2 with a "You need to bump this formula manually since the new
    URL and old URL are both: ..." error. This manually updates the
    formula accordingly.

--- Formula/r/rdkit.rb
+++ Formula/r/rdkit.rb
@@ -2,10 +2,9 @@ class Rdkit < Formula
   desc "Open-source chemoinformatics library"
   homepage "https://rdkit.org/"
   # NOTE: Make sure to update RPATHs if any "@rpath-referenced libraries" show up in `brew linkage`
-  url "https://github.com/rdkit/rdkit/archive/refs/tags/Release_2025_09_1.tar.gz"
-  sha256 "7fb3510b69af358009e2d0763c1d9665ac34f4c2cd3314cf5210ee3d5a33d501"
+  url "https://github.com/rdkit/rdkit/archive/refs/tags/Release_2025_09_2.tar.gz"
+  sha256 "db3254e88ef17cac7bb8480e6da9d1e69dfe180c21774b11637d8a17b5a98fb9"
   license "BSD-3-Clause"
-  revision 1
   head "https://github.com/rdkit/rdkit.git", branch: "master"
 
   livecheck do
